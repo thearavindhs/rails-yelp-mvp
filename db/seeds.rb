7fa6db7d5bbf83e2b67205fb86ccd970d3f0b995
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-01-05 22:59:13 -0500

    seeding the data completion

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  restaurant.save!
+  rand(1..5).times do
+    review = Review.new(
+      rating: rand(1..5),
+      content:  Faker::Restaurant.review,
+      restaurant:  restaurant
+    )
+    review.save!
+  end
+end
